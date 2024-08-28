45ab1e7fcf399eedabeb415d23942969b12e14ac
michael vasquez <mvasq12@lsu.edu>
2024-08-28 13:15:34 -0600

    first commit

---
+++ sketch.js
@@ -0,0 +1,37 @@
+function setup()
+{
+    createCanvas(600, 1200);
+}
+
+ function draw()
+ {
+        //color for bg
+    background(220);
+
+    for (var i = 0; i < 11; i++) 
+    {
+            //side-updown-size
+        //black 0 
+        fill('black');
+        ellipse(500, 100 + i * 100, 100);
+        fill('grey');
+        ellipse(400, 100 + i * 100, 70 );
+        fill(180);
+        ellipse(300, 100 + i * 100, 60 );
+        fill(200);
+        ellipse(200, 100 + i * 100, 40 );
+        fill(225);
+        ellipse(100, 100 + i * 100, 20 );
+        fill(235);
+        noStroke()
+        ellipse(40, 100 + i * 100, 6 );
+
+        //ellipse(100 + i * 150, 200 + i * 160, 100 - i * 10);
+        //ellipse(100 + i * 140 - 150, 200 + i * 161, 100 - i * 15);
+        //white 225
+            // fill('white')
+            // noStroke();
+            // ellipse(450, 300, 400);
+    }
+    
+ }
